deef63972adebfea377649b118214b4876a9a3e9
Frederik Voordeckers <frederik@cake.app>
2019-10-29 08:35:43 +0100

    fix index exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import useFlag from './hooks/useFlag';
 import FlagsContext from './components/FlagsContext';
-import FlagsProvider from './components/FlagsProvider';
-import FeatureFlag from './components/FeatureFlag';
+import FlagsProvider, { FlagsProviderProps } from './components/FlagsProvider';
+import FeatureFlag, { FeatureFlagComponentProps } from './components/FeatureFlag';
+import { FlagsConfig, FlagValue } from './api/FlagsApi';
 
 export {
     FeatureFlag,
+    FeatureFlagComponentProps,
     FlagsProvider,
+    FlagsProviderProps,
     FlagsContext,
     useFlag,
+    FlagsConfig,
+    FlagValue
 };
