9e23ecc4b59de54423529de98eb5c2c783cdb93d
Frederik Voordeckers <frederik@cake.app>
2019-10-30 13:33:54 +0100

    Add Feature flag component tests

---
+++ src/components/FeatureFlag.test.tsx
@@ -0,0 +1,62 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { FlagValue } from '../api/FlagsApi';
+import * as useFlag from '../hooks/useFlag';
+import FeatureFlag from './FeatureFlag';
+
+describe('FeatureFlag', () => {
+
+    it('renders an enabled feature', () => {
+        jest.spyOn(useFlag, 'default').mockImplementation((name: string) => {
+            return { name, enabled: true } as FlagValue;
+        });
+
+        const rendered = shallow(<FeatureFlag name='foo'>bar</FeatureFlag>);
+        expect(rendered.contains('bar')).toBeTruthy();
+    })
+
+    it('renders an disabled feature', () => {
+        jest.spyOn(useFlag, 'default').mockImplementation((name: string) => {
+            return { name, enabled: false } as FlagValue;
+        });
+
+        const rendered = shallow(<FeatureFlag name='foo'>bar</FeatureFlag>);
+        expect(rendered.contains('bar')).toBeFalsy();
+    })
+
+    it('renders the default fallback value', () => {
+        jest.spyOn(useFlag, 'default').mockImplementation(() => {
+            return undefined;
+        });
+
+        const rendered = shallow(<FeatureFlag name='foo'>bar</FeatureFlag>);
+        expect(rendered.contains('bar')).toBeFalsy();
+    })
+    
+    it('renders the default false value', () => {
+        jest.spyOn(useFlag, 'default').mockImplementation(() => {
+            return undefined;
+        });
+
+        const rendered = shallow(<FeatureFlag name='foo' defaultValue={false}>bar</FeatureFlag>);
+        expect(rendered.contains('bar')).toBeFalsy();
+    })
+
+    it('renders the default false value', () => {
+        jest.spyOn(useFlag, 'default').mockImplementation(() => {
+            return undefined;
+        });
+    
+        const rendered = shallow(<FeatureFlag name='foo' defaultValue={true}>bar</FeatureFlag>);
+        expect(rendered.contains('bar')).toBeTruthy();
+    })
+
+    it('renders an inverted feature', () => {
+        jest.spyOn(useFlag, 'default').mockImplementation(() => {
+            return { enabled: false } as FlagValue;
+        });
+    
+        const rendered = shallow(<FeatureFlag name='foo' invert={true}>bar</FeatureFlag>);
+        expect(rendered.contains('bar')).toBeTruthy();
+    })
+});
