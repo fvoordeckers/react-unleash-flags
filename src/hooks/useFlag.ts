9a0aefaa7dbe95efa16f7f0824043fe3ad77aee6
Frederik Voordeckers <frederik@cake.app>
2019-11-05 14:17:20 +0100

    1.0.2

--- src/hooks/useFlag.ts
+++ src/hooks/useFlag.ts
@@ -7,27 +7,27 @@ import FlagsContext from '../components/FlagsContext';
  * @param flagName the name of the flag
  */
 const useFlag = (flagName: string) => {
-    const [flagValue, setFlagValue] = useState<FlagValue>();
-    const flagsClient = useContext(FlagsContext);
+  const [flagValue, setFlagValue] = useState<FlagValue>();
+  const flagsClient = useContext(FlagsContext);
 
-    useEffect(() => {
-        if (flagsClient) {
-            if (!flagsClient.getFlags().length) {
-                // refresh the current flags set if there are no flags yet
-                const refresh = async () => {
-                    await flagsClient.init();
-                    const refreshedFlag = flagsClient.getFlag(flagName);
-                    setFlagValue(refreshedFlag);
-                };
-                refresh();
-            } else {
-                // return the current flag if available
-                const flag = flagsClient.getFlag(flagName);
-                setFlagValue(flag);
-            }
-        }
-    }, [flagsClient, flagName]);
-    return flagValue;
+  useEffect(() => {
+    if (flagsClient) {
+      if (!flagsClient.getFlags().length) {
+        // refresh the current flags set if there are no flags yet
+        const refresh = async () => {
+          await flagsClient.init();
+          const refreshedFlag = flagsClient.getFlag(flagName);
+          setFlagValue(refreshedFlag);
+        };
+        refresh();
+      } else {
+        // return the current flag if available
+        const flag = flagsClient.getFlag(flagName);
+        setFlagValue(flag);
+      }
+    }
+  }, [flagsClient, flagName]);
+  return flagValue;
 };
 
 export default useFlag;
