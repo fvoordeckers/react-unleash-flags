0703884dccf43697b0b32d99dfcda7f54a61501c
Shaun Stone <shauns@nutmeg.com>
2021-04-20 15:40:25 +0100

    added better test for fallback

--- src/client/FlagsClient.test.ts
+++ src/client/FlagsClient.test.ts
@@ -4,8 +4,7 @@ describe('FlagsClient', () => {
   const config: FlagsConfig = {
     appName: 'production',
     instanceId: 'foo',
-    url: 'https://foo.bar/api',
-    uri: '/client/features',
+    url: 'https://foo.bar/api'
   };
   const fakeFetch = jest.fn();
 
@@ -40,11 +39,11 @@ describe('FlagsClient', () => {
 
   it('constructs a FlagApi instance', () => {
     const flagsClient = new FlagsClient(config);
-    expect(JSON.stringify(flagsClient.config)).toEqual(JSON.stringify(config));
+    expect(JSON.stringify(flagsClient.config)).toEqual(JSON.stringify({ ...config, uri: '/client/features' }));
   });
 
   it('constructs a FlagApi instance with custom uri', () => {
-    const configWithCustomURI = { ...config, uri: 'custom_uri' };
+    const configWithCustomURI = { ...config, uri: '/custom_uri' };
     const flagsClient = new FlagsClient(configWithCustomURI);
     expect(JSON.stringify(flagsClient.config)).toEqual(JSON.stringify(configWithCustomURI));
   });
